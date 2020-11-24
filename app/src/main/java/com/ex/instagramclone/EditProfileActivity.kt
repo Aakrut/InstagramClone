00568402524c2755d472d019cc1494d1a35fe0d1
Aakrut Dabhi <67114280+Aakrut@users.noreply.github.com>
2020-11-24 21:55:10 +0530

    instagramclone

--- app/src/main/java/com/ex/instagramclone/EditProfileActivity.kt
+++ app/src/main/java/com/ex/instagramclone/EditProfileActivity.kt
@@ -1,11 +1,22 @@
 package com.ex.instagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ex.instagramclone.databinding.ActivityEditProfileBinding
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var editProfileBinding: ActivityEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        editProfileBinding = ActivityEditProfileBinding.inflate(layoutInflater)
+        val view = editProfileBinding.root
+        setContentView(view)
+
+        editProfileBinding.cancelEditProfile.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
     }
 }
